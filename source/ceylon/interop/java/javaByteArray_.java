cb120a0a9c1568505672a1d9ea6961cfa1dd43f8
gavinking <gavin@ceylon-lang.org>
2014-07-28 22:05:00 +0200

    make javaByteArray() accept an Array<Byte>

--- source/ceylon/interop/java/javaByteArray_.java
+++ source/ceylon/interop/java/javaByteArray_.java
@@ -9,6 +9,7 @@ import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Ignore;
 import com.redhat.ceylon.compiler.java.metadata.Method;
 import com.redhat.ceylon.compiler.java.metadata.Name;
+import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 
 @Ceylon(major = 7)
 @Method
@@ -19,14 +20,17 @@ public final class javaByteArray_ {
     private javaByteArray_() {}
 
     /**
-     * Cast a Ceylon <code>Array&lt;java.lang.Byte&gt;</code> to a Java 
-     * <code>ByteArray</code>, that is, to <code>byte[]</code>, preserving 
-     * the identity of the given array.
+     * Cast a Ceylon <code>Array&lt;ceylon.language.Byte&gt;</code> 
+     * or <code>Array&lt;java.lang.Byte&gt;</code> to a Java 
+     * <code>ByteArray</code>, that is, to <code>byte[]</code>, 
+     * preserving the identity of the given array.
      * 
      * @see ByteArray
      */
-    public static byte[] javaByteArray(@Name("array") Array<java.lang.Byte> array){
-        if(array.toArray() instanceof byte[]){
+    public static byte[] javaByteArray(@SuppressWarnings("rawtypes") @Name("array") 
+    @TypeInfo("ceylon.language::Array<ceylon.language::Byte>|ceylon.language::Array<java.lang::Byte>") 
+    Array array){
+        if (array.toArray() instanceof byte[]){
             return (byte[]) array.toArray();
         }
         throw new AssertionError("Invalid source array type: "+array.toArray());
