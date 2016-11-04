c035a7efce3ac9654ee5bb8f11fd5c3b6f645028
gavinking <gavin@ceylon-lang.org>
2016-11-04 12:58:41 +0100

    add JavaDoc

--- source/ceylon/interop/spring/CeylonRepository.java
+++ source/ceylon/interop/spring/CeylonRepository.java
@@ -7,6 +7,16 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
+/**
+ * A {@link CrudRepository) which supports use of Ceylon
+ * {@link ceylon.language.Integer} or
+ * {@link ceylon.language.String} as an identifier type,
+ * in place of Java's {@link java.lang.Long} or
+ * {@link java.lang.String}.
+ *
+ * @param <T> the entity type
+ * @param <ID> the identifier type
+ */
 @NoRepositoryBean
 @TypeParameters({@TypeParameter(value = "T"), @TypeParameter(value="ID")})
 public interface CeylonRepository<T, ID extends Serializable>
